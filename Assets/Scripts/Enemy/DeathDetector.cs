e69d8dff3e12dcd9e239e3f1f7585053e9b5e1d9
BearShorts11 <BearShorts11@users.noreply.github.com>
2025-10-23 19:23:16 -0500

    Various (DESC)
    Added death detector script for basic level events
    Changed Mock up into terrain for myself to test (still has probuilder in scene, don't worry)
    added a grass PNG for temp details test in scene
    Manifest files are for terrain tools verification

---
+++ Assets/Scripts/Enemy/DeathDetector.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DeathDetector : MonoBehaviour
+{
+    [SerializeField][Tooltip("Killable Enemy(s) that enable or disable an object.")]
+    private IEnemy TargetEnemy;
+    private GameObject toggledObject;
+
+    private bool enemyKilled = false;
+    private bool enableObject = false;
+    private bool finished = false;
+
+    private void Start()
+    {
+        finished = false;
+        enemyKilled = false;
+    }
+    void Update()
+    {
+        if(finished) Destroy(this);
+
+        if (EnemyCheck() && enableObject == false)
+        {
+            toggledObject.gameObject.SetActive(false);
+            finished = true;
+
+        }
+        else if (EnemyCheck() && enableObject == true)
+        {
+            toggledObject.gameObject.SetActive(true);
+            finished = true;
+        }
+    }
+
+    private bool EnemyCheck()
+    {
+        if (TargetEnemy) enemyKilled = false;
+        else if (!TargetEnemy) enemyKilled = true;
+        return enemyKilled;
+    }
+}
