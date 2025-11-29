58d340d3d0b1d8a3436de43c5bb538abd10193e7
That 1 Guy <394772@colum.edu>
2025-11-28 22:31:53 -0600

    Animator Clip Handler Script update

--- Assets/Scripts/Utility/AnimatorClipHandler.cs
+++ Assets/Scripts/Utility/AnimatorClipHandler.cs
@@ -23,46 +23,37 @@ public class AnimatorClipHandler : StateMachineBehaviour
         animCurrentTransitionTime = animator.GetAnimatorTransitionInfo(0).duration;
         if (nextAnimName != null) nameToIndexHash = Animator.StringToHash(nextAnimName);
         currentAnimNameToHash = stateInfo.fullPathHash;
+        float animLength = stateInfo.length + animCurrentTransitionTime;
+        animTotalLength = transitionIsPct ? stateInfo.normalizedTime - ((animLength - transitionTime) / animLength) : animLength - transitionTime;
 
-        EndAnim();
-        
-        IEnumerable EndAnim()
-        {
-            float animLength = stateInfo.length + animCurrentTransitionTime;
-            animTotalLength = transitionIsPct ? stateInfo.normalizedTime - ((animLength - transitionTime) / animLength) : animLength - transitionTime;
-
-            yield return new WaitForSeconds(animTotalLength);
-
-            if (nextAnimName == "" || stateInfo.fullPathHash != currentAnimNameToHash) yield break;
-
-            if (crossfade)
-            {
-                if (transitionIsPct && (stateInfo.length + transitionTime) > animTotalLength)
-                {
-                    float transitionLength = stateInfo.length * transitionTime;
-                    animator.CrossFade(nextAnimName, transitionLength, -1, 0, animTotalLength);
-                }
-                else
-                {
-                    animator.CrossFade(nextAnimName, transitionTime);
-                }
-            }
-            else
-            {
-                animator.Play(nextAnimName);
-            }
-        }
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {    }
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (nextAnimName == "" || animator.GetCurrentAnimatorStateInfo(0).fullPathHash != currentAnimNameToHash) return;
+
+        if (crossfade)
+        {
+            if (transitionIsPct && (stateInfo.length + transitionTime) > animTotalLength)
+            {
+                float transitionLength = stateInfo.length * transitionTime;
+                animator.CrossFade(nextAnimName, transitionLength, -1, 0, animTotalLength);
+            }
+            else
+            {
+                animator.CrossFade(nextAnimName, transitionTime);
+            }
+        }
+        else
+        {
+            animator.Play(nextAnimName);
+        }
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
