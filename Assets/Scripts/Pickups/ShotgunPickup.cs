717b87bdb4e17b60f12cbdf0a9672ef927191312
Roberto A. Ramirez <robaram726@gmail.com>
2026-02-27 17:32:24 -0600

    Gun Descends & Intro Dialogue is in.

--- Assets/Scripts/Pickups/ShotgunPickup.cs
+++ Assets/Scripts/Pickups/ShotgunPickup.cs
@@ -1,14 +1,23 @@
+using System.Collections;
 using FMODUnity;
 using UnityEngine;
 
 public class ShotgunPickup : IPickup
 {
     [SerializeField] private EventReference pickupSound;
+    [SerializeField] GameObject ApofinalPos;
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] GameObject shotgunPickup;
+
+    private Vector3 targetPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Player = FindFirstObjectByType<PlayerBehavior>();
+        targetPosition = ApofinalPos.transform.position;
+        StartCoroutine(MoveShotgun());
+
     }
 
     // Update is called once per frame
@@ -29,5 +38,17 @@ public class ShotgunPickup : IPickup
 
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator MoveShotgun()
+    {
+       // while (true)
+       // {
+           while ((targetPosition - shotgunPickup.transform.position).sqrMagnitude > 0.01f)
+           {
+                shotgunPickup.transform.position = Vector3.MoveTowards(shotgunPickup.transform.position, targetPosition, speed * Time.deltaTime);
+                yield return null;
+           }
+       // }
     }
 }
