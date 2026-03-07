e7902bf00caa164b48cf6a147e95dd3217f29aef
AlexPuntos <116763279+AlexPuntos@users.noreply.github.com>
2026-03-06 18:55:42 -0600

    Fog Controller Script
    Had a new script that can change fog at runtime so it doesn't ruin the vibe of the void and can dynamically change throughout a level

---
+++ Assets/Scripts/Utility/FogController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    public void FogEnable(bool check)
+    { 
+        RenderSettings.fog = check;
+        RenderSettings.fogMode = FogMode.ExponentialSquared;
+    }
+
+        public void FogColor(string hexCode)
+    {
+        ColorUtility.TryParseHtmlString(hexCode, out Color hexColor);
+        RenderSettings.fogColor = hexColor;
+    }
+    public void FogDensity(float density) => RenderSettings.fogDensity = density;
+}
