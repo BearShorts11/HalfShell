29553be5399842f000f239bdfaf9053b9a99bff8
AlexPuntos <116763279+AlexPuntos@users.noreply.github.com>
2026-03-18 19:23:53 -0500

    Lerp Fog
    Now the Fog Controller can be Lerped so that, when Fog is changed, it has the option to ease in based on a time set by designers.
    
    Best used with triggers. Always set the ChangeTime first.

--- Assets/Scripts/Utility/FogController.cs
+++ Assets/Scripts/Utility/FogController.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using System.Collections;
 
 public class FogController : MonoBehaviour
 {
+    public float ChangeTime = 0;
+    Coroutine FogChange;
+
     public void FogEnable(bool check)
     { 
         RenderSettings.fog = check;
@@ -10,7 +14,36 @@ public class FogController : MonoBehaviour
     public void FogColor(string hexCode)
     {
         ColorUtility.TryParseHtmlString(hexCode, out Color hexColor);
-        RenderSettings.fogColor = hexColor;
+        if (ChangeTime > 0) { StartCoroutine(LerpFog(hexColor, RenderSettings.fogDensity)); }
+        else { RenderSettings.fogColor = hexColor; }
     }
-    public void FogDensity(float density) => RenderSettings.fogDensity = density;
+    public void FogDensity(float density)
+    {
+        if (ChangeTime > 0) { StartCoroutine(LerpFog(RenderSettings.fogColor, density)); }
+        else { RenderSettings.fogDensity = density; }
+    }
+    public void SetChangeTime(float time) => ChangeTime = time; 
+
+    private IEnumerator LerpFog(Color color, float density)
+    {
+        Color startColor = RenderSettings.fogColor;
+        float startDensity = RenderSettings.fogDensity;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < ChangeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.SmoothStep(0f, 1f, elapsedTime / ChangeTime);
+
+            RenderSettings.fogColor = Color.Lerp(startColor, color, t);
+            RenderSettings.fogDensity = Mathf.Lerp(startDensity, density, t);
+
+            yield return null;
+        }
+
+        ChangeTime = 0f;
+        RenderSettings.fogColor = color;
+        RenderSettings.fogDensity = density;
+    }
 }
