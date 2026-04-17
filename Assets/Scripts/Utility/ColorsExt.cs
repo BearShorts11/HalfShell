62188ae30a027f589dcafff3a5bb7dcc28231bb9
VN-That1Guy <394772@colum.edu>
2026-04-16 23:39:19 -0500

    Update ColorsExt.cs

--- Assets/Scripts/Utility/ColorsExt.cs
+++ Assets/Scripts/Utility/ColorsExt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-// Not sure what this does but I'm assuming it's important to have it in judging from reading the decompiled code of Unity's Color class
+// A.K.A a class containing a static property list of colors that doesn't exist within Unity (like seriously, why *don't* they exist?)
 public class ColorsExt
 {
     /// <summary>
