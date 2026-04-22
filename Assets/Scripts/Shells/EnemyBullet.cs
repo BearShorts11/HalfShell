764941aee4aea3dbe57be9b69771104e0a143d3e
VN-That1Guy <394772@colum.edu>
2026-04-21 21:05:53 -0500

    Update EnemyBullet.cs

--- Assets/Scripts/Shells/EnemyBullet.cs
+++ Assets/Scripts/Shells/EnemyBullet.cs
@@ -8,6 +8,8 @@ public class EnemyBullet : MonoBehaviour
     public float speed = 150F;
     private Vector3 target;
     private float targetReached = 0.001f;
+    Vector3 nextPos;
+    Collider objectToBeHit;
 
     /// <summary>
     /// Prevents a double hit issue where it would damage the player twice
@@ -23,13 +25,23 @@ public class EnemyBullet : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // from the prediction by the end of this method: if there was something about to be hit, call this method
+        if (objectToBeHit != null) OnTriggerEnter(objectToBeHit);
+
         if (target != null)
         {
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
             if (Vector3.Distance(transform.position, target) <= targetReached) Destroy(this.gameObject);
-
         }
         else transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        nextPos = transform.position + (transform.forward * speed * Time.deltaTime);
+
+        //Predict if the bullet will hit the player or something by the next update
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, speed * Time.deltaTime, 1<<0|1<<4|1<<6|1<<7|1<<9|1<<12))
+        {
+            if (hit.collider != null)
+                objectToBeHit = hit.collider;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
