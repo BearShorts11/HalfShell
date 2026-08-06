a7c251d3e7e79c421e44177df53a49ca3311ae1c
VN-That1Guy <394772@colum.edu>
2026-08-05 20:00:35 -0500

    Sprite Particles reorganization, Schizo Vision Hints

---
+++ Assets/Scripts/UI/Canvas2DSpriteBehavior.cs
@@ -0,0 +1,153 @@
+using UnityEngine;
+
+// World Canvas Billboard Sprite Behavior
+public enum FadeMode
+{
+    None = 0,
+    FadeIn = 1,
+    FadeOut = 2
+}
+public class Canvas2DSpriteBehavior : MonoBehaviour
+{
+    public bool billboard = true;
+    [SerializeField] private bool hidden = false;
+    public bool bHidden { 
+        private set { hidden = value; } 
+        get { return hidden; } 
+    }
+    [Range(0f,5f)]public float fadeSpeed = 1f;
+    private float fadeTime
+    {
+        get {  return fadeSpeed * Time.fixedDeltaTime; }
+    }
+
+    [SerializeField] protected Canvas canvas;
+    [SerializeField] protected CanvasGroup canvasGroup;
+    Transform cam;
+    Vector3 initScale = Vector3.one;
+    private FadeMode transition = FadeMode.None;
+    private float time;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+        cam = Camera.main.transform;
+        canvas = canvas != null ? canvas : gameObject.GetComponent<Canvas>();
+        canvasGroup = canvasGroup != null ? canvasGroup : gameObject.GetComponent<CanvasGroup>();
+        initScale = canvas.transform.localScale;
+
+        if (canvasGroup != null)
+            canvasGroup.alpha = bHidden ? 0 : 1;
+
+        HidePrompt(bHidden);
+    }
+
+    public void SetFadeSpeed(float newFadeSpeed)
+    {
+        fadeSpeed = Mathf.Clamp(newFadeSpeed, 0f, 5f);
+    }
+
+    /// <summary>
+    /// To show or hide the sprite/prompt object smoothly by fading it in or out.
+    /// Sets transition mode instead of controlling the Canvas Group alpha directly.
+    /// </summary>
+    /// <param name="bHide">True: Fade the object out, False: Fade the object in</param>
+    public void HidePrompt(bool bHide)
+    {
+        bHidden = bHide;
+        //if (canvas != null)
+        //    canvas.enabled = bShow;
+
+        if (canvasGroup == null) return;
+
+        if (!bHide)
+        {
+            transition = FadeMode.FadeIn;
+        }
+        else
+        {
+            transition = FadeMode.FadeOut;
+        }
+    }
+
+    /// <summary>
+    /// Hide the sprite/prompt instantly with no fade out. Directly controls Canvas Group Alpha
+    /// </summary>
+    /// <param name="bHide"></param>
+    public void HidePromptInstant(bool bHide)
+    {
+        bHidden = bHide;
+
+        if (canvasGroup == null) return;
+
+        canvasGroup.alpha = bHide ? 0 : 1;
+
+        transition = FadeMode.None;
+    }
+
+    public void FadeInPrompt()
+    {
+        if (canvasGroup.alpha != 1)
+        {
+            canvasGroup.alpha += fadeTime;
+            canvasGroup.alpha = Mathf.Clamp01(canvasGroup.alpha);
+        }
+        else
+            transition = FadeMode.None;
+    }
+
+    public void FadeOutPrompt()
+    {
+        if (canvasGroup.alpha != 0)
+        {
+            canvasGroup.alpha -= fadeTime;
+            canvasGroup.alpha = Mathf.Clamp01(canvasGroup.alpha);
+        }
+        else
+            transition = FadeMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!bHidden && billboard)
+        {
+            // Billboard effect
+            // I looked up how to billboard and the tutorials pretty much go to the LookAt method
+            //  but the lookAt method can rotate the thing in the wrong direction if the player is
+            //  directly above or below it.
+            // Setting the transform.forward to the player camera is much better but the problem
+            //  remains the same.
+            // Ideally, this billboard effect should be disabled for when there is a proper UI art
+            //  and it has the shader material/sprite to set up the billboard effect.
+            {
+                //canvas.transform.LookAt(cam, Vector3.up);
+                canvas.transform.rotation = cam.rotation;
+                //canvas.transform.Rotate(0, 180, 0);
+            }
+            //canvas.transform.forward = -cam.forward;
+
+            // Scale the prompt element according to the player's distance to keep it noticeable AND readable
+            canvas.transform.localScale = initScale * Mathf.Clamp((Vector3.Distance(this.transform.position, Camera.main.transform.position)) * 0.2f, 0.05f, 2);
+        }
+        if (transition != FadeMode.None)
+        {
+            time += fadeTime;
+            if (time >= fadeTime)
+            {
+                switch (transition)
+                {
+                    case FadeMode.FadeIn:
+                        FadeInPrompt(); 
+                        break;
+                    case FadeMode.FadeOut:
+                        FadeOutPrompt();
+                        break;
+                    default:
+                        break;
+                }
+                time = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/SimpleTriggerTimed.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+[System.Serializable]
+public class TimedTriggerEvent
+{
+    // Activate after this amount of time passes
+    [Tooltip("Wait this long before the Unity executes this event")]
+    [field: SerializeField] public float time { get; private set; }
+    // Set of events to activate
+    [SerializeField] public UnityEvent[] triggerEvent = new UnityEvent[new()];
+}
+
+public enum TriggerMode
+{
+    Enter = 0,
+    Exit = 1,
+    OnKillAll = 2
+}
+
+/// <summary>
+/// Simple Triggers: Timed
+/// Only one of the three methods of activation can be used
+/// Very similar to SceneSequences, though I think this may be the better one.
+/// </summary>
+public class SimpleTriggerTimed : MonoBehaviour
+{
+    public TriggerMode mode = TriggerMode.Enter;
+    private float time = 0;
+    private float timer = 0;
+    private int eventNo = 0;
+    public bool triggerOnce = true;
+    private bool _triggered = false;
+    private bool triggered // This is a field to reset the cancelled bool to false when this gets retriggered
+    {
+        get {  return _triggered; }
+        set
+        {
+            // cannot set this to true again if it can only trigger once
+            if (triggerOnce && _triggered) { _triggered = false; return; }
+
+            if (value == true)
+            {
+                cancelled = false;
+            }
+            _triggered = value;
+        }
+    }
+    private bool cancelled = false;
+
+    [Header("Events - Sequence of events \nthat execute in a certain order \nafter a specified amount of time")]
+    [Header("Enter Collider Triggers")]
+    public List<string> onTriggerEnterTag = new();
+    public List<TimedTriggerEvent> onTriggerEnter = new();
+
+    [Header("Exit Collider Triggers")]
+    public List<string> onTriggerExitTag = new();
+    public List<TimedTriggerEvent> onTriggerExit = new();
+
+    [Header("Kill All of... Triggers")]
+    public int InitEnemyCounter;
+    public int EnemyDeathsCounter;
+    public List<TimedTriggerEvent> onKillAll = new();
+
+    private void Start()
+    {
+        // Set the timer for the first event of the selected trigger mode (redundant if the first event's time is set to 0 but better safe than sorry)
+        SetTimer();
+    }
+
+    private void SetTimer()
+    {
+        switch (mode)
+        {
+            case TriggerMode.Enter:
+                timer = onTriggerEnter[0].time;
+                break;
+            case TriggerMode.Exit:
+                timer = onTriggerExit[0].time;
+                break;
+            case TriggerMode.OnKillAll:
+                timer = onKillAll[0].time;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (triggered || mode != TriggerMode.Enter || (triggered && triggerOnce)) return;
+
+        if (onTriggerEnter.Count == 1 && onTriggerEnterTag.Count == 0) { triggered = true; return; }
+
+        if (onTriggerEnter.Count != onTriggerEnterTag.Count) { Debug.Log("Trigger Enter tags don't match"); return; }
+
+        for (int i = 0; i < onTriggerEnter.Count; i++)
+        {
+            if (other.CompareTag(onTriggerEnterTag[i])) { triggered = true; break; }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (triggered || mode != TriggerMode.Exit || (triggered && triggerOnce)) return;
+
+        if (onTriggerExit.Count == 1 && onTriggerExitTag.Count == 0) { triggered = true; return; }
+
+        if (onTriggerExit.Count != onTriggerExitTag.Count) { Debug.Log("Trigger Exit tags don't match"); return; }
+
+        for (int i = 0; i < onTriggerExit.Count; i++)
+        {
+            if (other.CompareTag(onTriggerExitTag[i])) { triggered = true; break; }
+        }
+    }
+
+    public void ArenaKillCounter(int value)
+    {
+        if (triggered && triggerOnce) return;
+
+        EnemyDeathsCounter += value;
+
+        if (!triggered && EnemyDeathsCounter >= InitEnemyCounter && mode == TriggerMode.OnKillAll)
+        {
+            triggered = true;
+            //for (int i = 0; i < onKillAll.Count; i++) { onKillAll[i].Invoke(); }
+        }
+    }
+
+    public void CancelTrigger()
+    {
+        if (cancelled & triggerOnce) return;
+
+        cancelled = true;
+        triggered = false;
+        eventNo = 0;
+        time = 0;
+        SetTimer();
+    }
+
+    public void Trigger(List<TimedTriggerEvent> TimedEvents, int i)
+    {
+        // Activate the events in this index
+        for (int j = 0; j < TimedEvents[i].triggerEvent.Length; j++)
+        {
+            TimedEvents[i].triggerEvent[j].Invoke();
+        }
+
+        // Set the timer for the next index Event timer
+        i++;
+        if (i >= TimedEvents.Count) // If this is the final event that was triggered, stop the timer by setting triggered to false
+        { 
+            triggered = false;
+            i = 0; // Reset to the first event if this is a repeatable trigger
+        }
+        timer = TimedEvents[i].time;
+    }
+
+    public void Update()
+    {
+        if (!triggered) return;
+
+        time += Time.deltaTime;
+
+        if (time > timer)
+        {
+            switch (mode)
+            {
+                case TriggerMode.Enter:
+                    Trigger(onTriggerEnter, eventNo);
+                    break;
+                case TriggerMode.Exit:
+                    Trigger(onTriggerExit, eventNo);
+                    break;
+                case TriggerMode.OnKillAll:
+                    Trigger(onKillAll, eventNo);
+                    break;
+                default:
+                    break;
+            }
+            time = 0;
+            eventNo++;
+        }
+
+    }
+
+}
